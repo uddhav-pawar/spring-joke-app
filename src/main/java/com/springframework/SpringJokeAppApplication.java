b6fbe5bdcdda6e78e754509a0eda13331d6ec3b4
Uddhav <uddhavp0@gmail.com>
2024-02-17 17:41:12 +0530

    my first commit

---
+++ src/main/java/com/springframework/SpringJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokeAppApplication.class, args);
+	}
+
+}
